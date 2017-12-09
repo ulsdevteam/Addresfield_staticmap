e179f3b418f03dab38b3f7230afe7469c2c9025d
git <git@2552384.no-reply.drupal.org>
2017-12-09 22:34:34 +1100

    Issue #2198607 by matysek145, raul_drupal_dev: Multiple maps on same page using views

--- addressfield-staticmap-google-map.tpl.php
+++ addressfield-staticmap-google-map.tpl.php
@@ -4,37 +4,38 @@
   <div><?php print $settings['text_address']; ?></div>
 <?php endif; ?>
 
-<div id="map_canvas" style="width: <?php print $size[0]; ?>px; height: <?php print $size[1]; ?>px;">
+<div id="map_canvas<?php if(isset($settings['id'])) print '-'.$settings['id']; ?>" style="width: <?php print $size[0]; ?>px; height: <?php print $size[1]; ?>px;">
   <noscript><?php print $image; ?></noscript>
 </div>
 
 <script type="text/javascript">
+var staticMapGoogleMap<?php if(isset($settings['id'])) print '_'.$settings['id']; ?> = new function() {
   var address = '<?php print $address; ?>';
 
   var myOptions = {
-    zoom: <?php print $settings['zoom']; ?>,
+  zoom: <?php print $settings['zoom']; ?>,
     mapTypeId: google.maps.MapTypeId.<?php print strtoupper($settings['maptype']); ?>
     //disableDefaultUI: true,
     //scrollwheel: false,
     //draggable: false,
     //disableDoubleClickZoom: true
   }
-  var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-  
+  var map = new google.maps.Map(document.getElementById('map_canvas<?php if(isset($settings['id'])) print '-'.$settings['id']; ?>'), myOptions);
+
   <?php if (isset($settings['info_window'])): ?>
-    // info window
-    var content = document.createElement('div');
-    content.className = 'info-window';
-    content.innerHTML = '<?php print $settings['text_address']; ?>';
-    
-    var infowindow = new google.maps.InfoWindow({
-      content: content
-    });
+  // info window
+  var content = document.createElement('div');
+  content.className = 'info-window';
+  content.innerHTML = '<?php print $settings['text_address']; ?>';
+
+  var infowindow = new google.maps.InfoWindow({
+  content: content
+  });
   <?php endif; ?>
-  
+
   <?php foreach ($kml_paths as $kml_path): ?>
-    var ctaLayer = new google.maps.KmlLayer('<?php print $kml_path; ?>');
-    ctaLayer.setMap(map);
+  var ctaLayer = new google.maps.KmlLayer('<?php print $kml_path; ?>');
+  ctaLayer.setMap(map);
   <?php endforeach; ?>
 
   var geocoder = new google.maps.Geocoder();
@@ -43,15 +44,16 @@
     if (status == google.maps.GeocoderStatus.OK) {
       map.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
-        map: map,
+      map: map,
         position: results[0].geometry.location
       });
       <?php if (isset($settings['info_window'])): ?>
-        // info window
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map,marker);
-        });
+      // info window
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+      });
       <?php endif; ?>
     }
- });
+  });
+}
 </script>
